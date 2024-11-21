0d358ddecdd65ec99ab92de935b7b7267e8ae737
luopeng <luopeng101297@163.com>
2024-11-21 16:17:28 +0800

    feat: element ui汉化

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,8 @@
 import ElementPlus from './el-element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 export default {
   install: (app) => {
-    app.use(ElementPlus);
+    app.use(ElementPlus, { locale: zhCn });
   }
 }
